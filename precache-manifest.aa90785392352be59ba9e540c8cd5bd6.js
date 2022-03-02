ac102473e01af8b263f4d273b1637a129a665440
hs2119 <hs2119@ic.ac.uk>
2022-03-02 03:21:56 +0000

    Updates

---
+++ precache-manifest.aa90785392352be59ba9e540c8cd5bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4432184cc4444e943f5e752a000a471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e033d58f55d6737f1303",
+    "url": "/static/css/main.35c37fcf.chunk.css"
+  },
+  {
+    "revision": "592c98d0c92d900968d9",
+    "url": "/static/js/2.5894bd05.chunk.js"
+  },
+  {
+    "revision": "88557a10b562e7f4d59ea42929ebcb0d",
+    "url": "/static/js/2.5894bd05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e033d58f55d6737f1303",
+    "url": "/static/js/main.d478c0eb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
