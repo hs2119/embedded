599585f0eb3b550ac5a01e12ef496b4450368d0a
hs2119 <hs2119@ic.ac.uk>
2022-03-02 03:42:36 +0000

    Updates

---
+++ precache-manifest.a5ddafa2d2882457f1eafc19ae89056c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f1a8c6285942849476ac12baf5be1e",
+    "url": "/embedded/index.html"
+  },
+  {
+    "revision": "4c46393c760e812f13b6",
+    "url": "/embedded/static/css/main.35c37fcf.chunk.css"
+  },
+  {
+    "revision": "4b52311faa9499939bf6",
+    "url": "/embedded/static/js/2.81f97c76.chunk.js"
+  },
+  {
+    "revision": "88557a10b562e7f4d59ea42929ebcb0d",
+    "url": "/embedded/static/js/2.81f97c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c46393c760e812f13b6",
+    "url": "/embedded/static/js/main.f120d881.chunk.js"
+  },
+  {
+    "revision": "ea1552b42be91980f8fb",
+    "url": "/embedded/static/js/runtime-main.07f184df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa90785392352be59ba9e540c8cd5bd6.js"
+  "/embedded/precache-manifest.a5ddafa2d2882457f1eafc19ae89056c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/embedded/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
